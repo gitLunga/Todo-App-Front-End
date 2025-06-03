3d4e5eb87e22a40845931a850191fda484a727ed
gitLunga <lungamngomezulu10@gmail.com>
2025-06-03 13:46:51 +0200

    pages and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Register from './screens/Register';
 import Login from './screens/Login';
-import PlayerForm from './component/PlayerForm';
+
+import TaskItem from './component/TaskItem';
+import TaskList from './component/TaskList';
+import TaskForm from './component/TaskForm';  
+import FilterControls from './component/FilterControls';
 
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Register />} />
         <Route exact path='/login' element={<Login />} />
-        <Route exact path='/playerform' element={<PlayerForm />} />
+
+
+        <Route exact path='/TaskItem' element={<TaskItem />} />
+        <Route exact path='/TaskList' element={<TaskList/>} />
+        <Route exact path='/TaskForm' element={<TaskForm />} />
+        <Route exact path='/Filter' element={<FilterControls />} />
 
 
       </Routes>
